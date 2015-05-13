75f4895457d3e01e4486337647c9968251439a94
jlherlein <jlherlein@hotmail.com>
2015-05-12 22:32:54 -0300

    UPDATE ctm

--- punto2.m
+++ punto2.m
@@ -1,3 +1,4 @@
+format long;
 A=50;
 C=500;
 M=20;
@@ -8,39 +9,36 @@ deltaT=0.125;
 deltaCg=0.8;
 beta=C/M;
 tau=C/(K*A);
-iter=100000;
+iter=100;
 tf=deltaT*iter;
 kh=0.1;
 kv=1;
-kc=500;
+kc=100;
 kp=M/(K*A);
 v=25;
 c1=0;
-global m1;
 m1=0;
 m=0;
 c=20;
 constanteK=kv*kc*kh*kp;
 betaLC=beta/(kv*kc*kh);
 tauLC=tau/(1+constanteK);
-
-
 r=kh*v; %r=ka*v y kh=ka;
-tfinal=0;
+tiempo=0;
 e=r-c1;
-i=0;
+funcion=@(m1,y)(m1/beta)-((y-Te)/tau);
+
 for i=1:iter
-    funcion=@(t,y)(m1/betaLC)-((y-Te)/tauLC);
-    tinic=tfinal;
-    tfinal=tinic+deltaT;
+    a(i)=tiempo;
+    b(i)=c;
     e=r-c1
     m=kc*e;
     m1=kv*m;
-    [t,y]=metodoEuler(funcion,tinic,tfinal,c,iter);
-    c=y(iter);
+    Cnuevo=c+deltaT*funcion(m1,c);
+    c=Cnuevo;
     c1=kh*c;
-    a(i)=tfinal;
-    b(i)=c;
+   
+    tiempo=tiempo+deltaT;
 end
-
+eehhloco=1/(1+constanteK)
 plot(a,b);
